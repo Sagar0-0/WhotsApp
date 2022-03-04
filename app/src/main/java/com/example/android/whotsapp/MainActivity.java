634648260d6a4e49710792273a5d8ac8cf4018ff
Sagar <sagar.0dev@gmail.com>
2022-03-04 20:01:16 +0530

    Home UI

--- app/src/main/java/com/example/android/whotsapp/MainActivity.java
+++ app/src/main/java/com/example/android/whotsapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.android.whotsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.android.whotsapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
