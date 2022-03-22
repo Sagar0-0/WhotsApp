5a92fdf440b8d89076547f47b9103af8243f78a8
Sagar <sagar.0dev@gmail.com>
2022-03-23 02:02:41 +0530

    Frags

--- app/src/main/java/com/example/android/whotsapp/MainActivity.java
+++ app/src/main/java/com/example/android/whotsapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.android.whotsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 import com.example.android.whotsapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +26,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+    private void setUpWithViewPager(ViewPager viewPager){
+
+    }
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList=new ArrayList<>();
+        private final List<String> mFragmentTitleList=new ArrayList<>();
+
+        public SectionsPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment,String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
     }
 }
