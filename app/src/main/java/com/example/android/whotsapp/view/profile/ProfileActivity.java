13919e0f50358a6f035482d6b4a5f40e0e27cbc1
Sagar <sagar.0dev@gmail.com>
2022-05-19 20:30:08 +0530

    image picker added

--- app/src/main/java/com/example/android/whotsapp/view/profile/ProfileActivity.java
+++ app/src/main/java/com/example/android/whotsapp/view/profile/ProfileActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.whotsapp.view.profile;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
@@ -18,6 +23,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.android.whotsapp.R;
 import com.example.android.whotsapp.databinding.ActivityProfileBinding;
@@ -29,6 +35,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.net.URI;
 import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -37,6 +44,8 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firestore;
     private BottomSheetDialog bottomSheetDialog;
+    private int IMAGE_GALLERY_REQUEST=420;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,18 +63,47 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void showBottomSheet() {
-        bottomSheetDialog = new BottomSheetDialog(this);
         View view=getLayoutInflater().inflate(R.layout.profile_pick_sheet,null);
+
+        view.findViewById(R.id.ll_gallery).setOnClickListener(v -> {
+            openGallery();
+            bottomSheetDialog.dismiss();
+        });
+        view.findViewById(R.id.ll_camera).setOnClickListener(v -> {
+            Toast.makeText(this, "Can't you wait for it?", Toast.LENGTH_SHORT).show();
+            bottomSheetDialog.dismiss();
+        });
+
+        bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(view);
 
         bottomSheetDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                bottomSheetDialog=null;
-            }
-        });
+        bottomSheetDialog.setOnDismissListener(dialog -> bottomSheetDialog=null);
         bottomSheetDialog.show();
+    }
+
+    private void openGallery() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Image"),IMAGE_GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==IMAGE_GALLERY_REQUEST
+        && resultCode==RESULT_OK
+        && data !=null
+        && data.getData()!=null){
+            imageUri=data.getData();
+            try{
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),imageUri);
+                binding.imageProfile.setImageBitmap(bitmap);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     private void getinfo() {
