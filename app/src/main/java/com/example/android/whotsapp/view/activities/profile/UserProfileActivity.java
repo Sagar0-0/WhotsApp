1ae733429fa96da40f2941a203adb75de33bea13
Sagar <sagar.0dev@gmail.com>
2022-06-07 19:51:41 +0530

    user profile action bar

--- app/src/main/java/com/example/android/whotsapp/view/activities/profile/UserProfileActivity.java
+++ app/src/main/java/com/example/android/whotsapp/view/activities/profile/UserProfileActivity.java
@@ -1,23 +1,29 @@
 package com.example.android.whotsapp.view.activities.profile;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.content.Intent;
-import android.os.Bundle;
-
 import com.bumptech.glide.Glide;
 import com.example.android.whotsapp.R;
-import com.example.android.whotsapp.databinding.ActivityProfileBinding;
 import com.example.android.whotsapp.databinding.ActivityUserProfileBinding;
+
+import java.util.Objects;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     private ActivityUserProfileBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_user_profile);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_profile);
 
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
@@ -26,11 +32,32 @@ public class UserProfileActivity extends AppCompatActivity {
 
         if (receiverId != null) {
             binding.toolbar.setTitle(userName);
-            if(userProfile==null || userProfile.equals("")){
+            if (userProfile == null || userProfile.equals("")) {
                 binding.imageProfile.setImageResource(R.drawable.profile_placeholder);
-            }else{
+            } else {
                 Glide.with(this).load(userProfile).into(binding.imageProfile);
             }
         }
+        initToolbar();
+    }
+    public void initToolbar(){
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            finish();
+        }else{
+            Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
