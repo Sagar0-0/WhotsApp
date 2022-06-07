06eaf6a69e07da792fec2fbd33fd745ee53c514c
Sagar <sagar.0dev@gmail.com>
2022-06-07 20:06:52 +0530

    peofile in status frag

--- app/src/main/java/com/example/android/whotsapp/menu/StatusFragment.java
+++ app/src/main/java/com/example/android/whotsapp/menu/StatusFragment.java
@@ -2,65 +2,57 @@ package com.example.android.whotsapp.menu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.example.android.whotsapp.R;
+import com.example.android.whotsapp.databinding.FragmentStatusBinding;
+import com.example.android.whotsapp.view.activities.profile.ProfileActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatusFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StatusFragment extends Fragment {
+    public StatusFragment(){}
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StatusFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatusFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatusFragment newInstance(String param1, String param2) {
-        StatusFragment fragment = new StatusFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private FragmentStatusBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+
+        binding= DataBindingUtil.inflate(inflater,R.layout.fragment_status, container, false);
+
+        getProfile();
+        return binding.getRoot();
+    }
+
+    private void getProfile() {
+        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore firestore=FirebaseFirestore.getInstance();
+        firestore.collection("Users").document(firebaseUser.getUid()).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    String imageProfile = documentSnapshot.getString("imageProfile");
+
+                    assert imageProfile != null;
+                    if(imageProfile.equals("")){
+                        binding.imageProfile.setImageResource(R.drawable.profile_placeholder);
+                    }else{
+                        Glide.with(getContext()).load(imageProfile).into(binding.imageProfile);
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Get data", "onFailure: " + e.getMessage());
+            }
+        });
     }
 }
